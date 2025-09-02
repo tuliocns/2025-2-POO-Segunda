c63538c2e12abfc4f9201fb02a1dbce3070a56f3
Túlio Nassif <163613757+tuliocns@users.noreply.github.com>
2025-09-02 00:34:02 +0000

    feat: 2025-2-POO-Segunda.csproj

--- Program.cs
+++ Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+Televisao tv = new Televisao(55f);
+
+Console.WriteLine($"A tv tem  o tamanho {tv.Tamanho}");
+
+tv.Volume = -35;
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.Volume = 120;
+Console.WriteLine($"Volume {tv.Volume}");
