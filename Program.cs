9c701dcba94f7681a555859249943e89917c219d
Túlio Nassif <163613757+tuliocns@users.noreply.github.com>
2025-09-09 00:48:04 +0000

    2025-2-POO-Segunda

--- Program.cs
+++ Program.cs
@@ -1,9 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+//Console.WriteLine("Hello, World!");
 
 Televisao tv = new Televisao(55f);
 
-Console.WriteLine($"A tv tem  o tamanho {tv.Tamanho}");
+Console.WriteLine($"A tv tem o tamanho {tv.Tamanho}");
 
 tv.AumentarVolume();
 Console.WriteLine($"Volume {tv.Volume}");
@@ -13,3 +13,30 @@ Console.WriteLine($"Volume {tv.Volume}");
 
 tv.AumentarVolume();
 Console.WriteLine($"Volume {tv.Volume}");
+
+tv.DiminuirVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.DiminuirVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+tv.DiminuirVolume();
+Console.WriteLine($"Volume {tv.Volume}");
+
+
+tv.AumentarCanal();
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.AumentarCanal();
+Console.WriteLine($"Canal {tv.Canal}");
+
+
+tv.DiminuirCanal();
+Console.WriteLine($"Canal {tv.Canal}");
+
+tv.DiminuirCanal();
+Console.WriteLine($"Canal {tv.Canal}");
+
+
+tv.CanalEspecifico(520);
+Console.WriteLine($"Canal {tv.Canal}");
--- Televisao.cs
+++ Televisao.cs
@@ -1,13 +1,15 @@
 public class Televisao
 {
     private const int VOL_MAX = 100; // CONSTANTE
+    private int CANAL_ANTES_DESLIGAR = 0;
+    private const int CAN_MAX = 520; // CONSTANTE
 
     public Televisao(float tamanho)
     {
         Tamanho = tamanho;
     }
     
-    public float Tamanho { get; }
+    public float Tamanho { get;}
     public int Resolucao { get; set;}
     public int Volume { get; private set;}
     public int Canal { get; set;}
@@ -15,9 +17,56 @@ public class Televisao
 
     public void AumentarVolume()
     {
-        if(Volume < VOL_MAX)
-            Volume = Volume + 1;
+        if (Volume < VOL_MAX)
+            Volume = Volume + 1;   
         else
             Console.WriteLine("TV já está no máximo.");
     }
+
+    public void DiminuirVolume()
+    {
+        if (Volume > 0)
+            Volume--;
+        else
+            Console.WriteLine("TV já está no mínimo.");
+    }
+
+    public void LigarTV()
+    {
+        Estado = true;
+        Canal = CANAL_ANTES_DESLIGAR;
+    }
+
+    public void DesligarTV()
+    {
+        Estado = false;
+        CANAL_ANTES_DESLIGAR = Canal;
+    }
+
+    
+
+    public void AumentarCanal()
+    {
+        if (Canal < CAN_MAX)
+            Canal++;   
+        else
+            Console.WriteLine("O canal não existe.");
+    }
+
+    public void DiminuirCanal()
+    {
+        if (Canal > 1)
+            Canal--;
+        else
+            Console.WriteLine("Canal inexistente.");
+    }
+
+    public void CanalEspecifico(int CanalDesejado)
+    {
+        if (CanalDesejado < 1 || CanalDesejado > 520)
+            Console.WriteLine("Erro.");
+        else
+            Canal = CanalDesejado;
+    } 
+
 }
