c982dfaf8e5979f549fb9732602a743fa1748625
Túlio Nassif <163613757+tuliocns@users.noreply.github.com>
2025-09-16 00:09:53 +0000

    feat: 2025-2-POO-Segunda

--- heranca/CartaoDebito.cs
+++ heranca/CartaoDebito.cs
@@ -2,12 +2,16 @@ public class CartaoDebito
 {
     public CartaoDebito()
     {
-        Bandeira = "Visa";
-        Bandeira = "MinhaBandeira";
+        //Bandeira = "Visa";
+        //Bandeira = "MinhaBandeira";
         Bandeira = BandeiraCartao.Visa;
     }
-    public string Numero {get; set;}
-    public string Bandeira {get; set;}
+    public string Numero { get; set; }
+    //public string Bandeira {get; set;}
+    public BandeiraCartao Bandeira { get; set; }
+    public DateTime Vencimento { get; set; }
+    public string Portador { get; set; }
+    public string Cvv { get; set; }
 }
 
 public enum BandeiraCartao
