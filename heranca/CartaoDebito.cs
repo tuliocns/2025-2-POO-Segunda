fec4228abd01bdec0c6d4030a1ee9858155b3207
Túlio Nassif <163613757+tuliocns@users.noreply.github.com>
2025-09-16 00:35:45 +0000

    feat: 2025-2-POO-Segunda

--- heranca/CartaoDebito.cs
+++ heranca/CartaoDebito.cs
@@ -12,6 +12,23 @@ public class CartaoDebito
     public DateTime Vencimento { get; set; }
     public string Portador { get; set; }
     public string Cvv { get; set; }
+
+    public bool Debitar(decimal valor)
+    {
+        if (valor > Saldo)
+            return false;
+
+        Saldo -= valor;
+        return true;
+    }
+
+    public bool Debitar(decimal valor, bool Debitar)
+    {
+        if (valor <= (Saldo + valor))
+            return true;
+        
+    }
+
 }
 
 public enum BandeiraCartao
