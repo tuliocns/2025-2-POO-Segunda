6bd2d5911530a4af89625cdeed8a67f91fa29a3f
Túlio Nassif <163613757+tuliocns@users.noreply.github.com>
2025-09-30 00:30:39 +0000

    2025-2-POO-Segunda

---
+++ sobrescrita/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Globalization;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+CultureInfo.CurrentCulture = new CultureInfo("pt-BR");
+
+Cartao cartaoBase = new Cartao();
+cartaoBase.Debitar(100);
+
+Console.WriteLine("-- Cartão de Débito --");
+
+Cartao cartaoDebitao = new CartaoDebito();
+cartaoDebitao.Debitar(100);
